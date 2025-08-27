e19bfa2d99356d3e13b18104e4ea5badde55e114
Ayoub Amer <ayouub.ameur@gmail.com>
2025-08-27 15:48:59 +0100

    feat: switch to using polling event fetcher architecture

--- backend/src/fetcher/mint.rs
+++ backend/src/fetcher/mint.rs
@@ -36,6 +36,74 @@ pub async fn mint_nft_fetcher(app_state: &WebAppState) -> Result<()> {
     tracing::info!("Starting mint nft fetcher...");
 
     // Create the provider.
+    let rpc_url = &APP_CONFIG.alchemy_rpc_url;
+
+    let provider = ProviderBuilder::new().connect_http(rpc_url.parse()?);
+
+    let contract_address = Address::from_str(ENCLAVA_CONTRACT_ADDRESS)?;
+    let event_sig = "DatasetNFTMinted(address,uint256,string)";
+
+    let mut last_block = provider.get_block_number().await?;
+
+    loop {
+        let current_block = provider.get_block_number().await?;
+
+        if current_block >= last_block {
+            let filter = Filter::new()
+                .address(contract_address)
+                .event(event_sig)
+                .from_block(last_block);
+
+            let filtered_logs = provider.get_logs(&filter).await?;
+
+            for log in filtered_logs {
+                tracing::info!("New DatasetNFTMinted event: {:?}", log);
+
+                let log_data = log.data();
+
+                let dataset_nft: DatasetNFTMint;
+
+                // Try decode as DatasetNFTMinted
+                if let Ok(event) = DatasetNFTMinted::decode_log_data(&log_data) {
+                    tracing::trace!(
+                        "New NFT Minted! to: {:?}, tokenId: {:?}, datasetId: {}",
+                        event.to,
+                        event.tokenId,
+                        event.datasetId
+                    );
+
+                    dataset_nft = DatasetNFTMint {
+                        to: event.to,
+                        token_id: event.tokenId,
+                        dataset_id: event.datasetId,
+                        tx_hash: log.transaction_hash,
+                    };
+
+                    tracing::info!("DatasetNFTMinted: {:?}", dataset_nft);
+
+                    // Open new thraed that will handle the event(by inserting teh payment details in the database)
+                    let app_state = app_state.clone();
+
+                    tokio::spawn(async move {
+                        if let Err(e) = handle_new_nft_mint(&app_state, &dataset_nft).await {
+                            tracing::error!("Failed to handle new nft mint: {}", e);
+                        }
+                    });
+                }
+            }
+
+            last_block = current_block;
+        }
+
+        // Sleep for a while before polling again
+        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    }
+}
+
+pub async fn _mint_nft_fetcher_ws(app_state: &WebAppState) -> Result<()> {
+    tracing::info!("Starting mint nft fetcher...");
+
+    // Create the provider.
     let rpc_url = APP_CONFIG.alchemy_ws_url.clone();
     let ws = WsConnect::new(rpc_url);
     let provider = ProviderBuilder::new().connect_ws(ws).await?;
