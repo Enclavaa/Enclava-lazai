a52dc7186f5a2202ff3664f3500068fe31ec8550
Ayoub Amer <ayouub.ameur@gmail.com>
2025-08-27 14:36:12 +0100

    feat: add deploy script for EnclavaPayments and configure RPC endpoint

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -4,7 +4,7 @@ echo "Deploying EnclavaPayments to Duckchain..."
 
 forge script script/DeployEnclavaPayments.s.sol:DeployEnclavaPayments \
   --rpc-url duckchain_mainnet \
-  --broadcast \
+  --broadcast 
 
 
 echo -e "\n\nDeployed EnclavaPayments to Duckchain!"
